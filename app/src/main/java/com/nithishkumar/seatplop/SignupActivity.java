6a3e951db24ba56b642799b6fe8ce93ee7a57bb1
Nithish1201 <nithishnit2002@gmail.com>
2021-03-09 22:36:06 +0530

    creating layout of signup activity and making variables and hooks in login activity for making animation

--- app/src/main/java/com/nithishkumar/seatplop/SignupActivity.java
+++ app/src/main/java/com/nithishkumar/seatplop/SignupActivity.java
@@ -3,12 +3,34 @@ package com.nithishkumar.seatplop;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class SignupActivity extends AppCompatActivity {
+
+    //variables
+
+    ImageView backBtn;
+    Button next, login;
+    TextView titleText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+
+        //hooks
+        backBtn = findViewById(R.id.signup_back_button);
+        next = findViewById(R.id.signup_next_button);
+        login = findViewById(R.id.signup_login_button);
+        titleText = findViewById(R.id.signup_title_text);
+    }
+
+    public void callNextSignupScreen(View view) {
+
+
+
     }
 }
