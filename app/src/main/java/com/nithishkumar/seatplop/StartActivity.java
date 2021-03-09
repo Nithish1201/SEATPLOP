6e03758c2b5ffb351a69e3113d85c45d8d346fa6
Nithish1201 <nithishnit2002@gmail.com>
2021-03-09 13:08:41 +0530

    start activity design- animation

--- app/src/main/java/com/nithishkumar/seatplop/StartActivity.java
+++ app/src/main/java/com/nithishkumar/seatplop/StartActivity.java
@@ -4,13 +4,25 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 public class StartActivity extends AppCompatActivity {
+
+    //variables
+    Animation topAnim;
+    ImageView image;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_start);
+
+        topAnim = AnimationUtils.loadAnimation(this,R.anim.start_activity_top_animation);
+        image = findViewById(R.id.imageView2);
+
+        image.setAnimation(topAnim);
     }
 }
