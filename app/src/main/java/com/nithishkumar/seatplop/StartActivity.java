676c427b792e8c0570a06be64087f67f4a984e9c
Nithish1201 <nithishnit2002@gmail.com>
2021-03-09 14:08:02 +0530

    start activity design-animation - completed --- skipping to login activity

--- app/src/main/java/com/nithishkumar/seatplop/StartActivity.java
+++ app/src/main/java/com/nithishkumar/seatplop/StartActivity.java
@@ -2,7 +2,9 @@ package com.nithishkumar.seatplop;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.WindowManager;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
@@ -10,9 +12,13 @@ import android.widget.ImageView;
 
 public class StartActivity extends AppCompatActivity {
 
+    private static int SPLASH_SCREEN = 5000;
     //variables
-    Animation topAnim;
-    ImageView image;
+    Animation topAnim , bottAnim , leftanim , rgtanim;
+    ImageView image1;
+    ImageView image2;
+    ImageView image3;
+    ImageView image4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,8 +27,27 @@ public class StartActivity extends AppCompatActivity {
         setContentView(R.layout.activity_start);
 
         topAnim = AnimationUtils.loadAnimation(this,R.anim.start_activity_top_animation);
-        image = findViewById(R.id.imageView2);
+        bottAnim = AnimationUtils.loadAnimation(this,R.anim.start_activity_bottom_animation);
+        leftanim =  AnimationUtils.loadAnimation(this,R.anim.start_activity_left_animation);
+        rgtanim = AnimationUtils.loadAnimation(this,R.anim.start_activity_right_animation);
 
-        image.setAnimation(topAnim);
+        image1 = findViewById(R.id.imageView1);
+        image2 = findViewById(R.id.imageView2);
+        image3 = findViewById(R.id.imageView3);
+        image4 = findViewById(R.id.imageView4);
+
+        image1.setAnimation(topAnim);
+        image2.setAnimation(bottAnim);
+        image3.setAnimation(leftanim);
+        image4.setAnimation(rgtanim);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(StartActivity.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },SPLASH_SCREEN);
     }
 }
