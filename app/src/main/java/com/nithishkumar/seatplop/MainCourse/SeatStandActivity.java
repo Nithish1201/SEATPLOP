5f0dc5f1c4940f04a8317e7b23746b6dc4bd3176
Nithish1201 <nithishnit2002@gmail.com>
2021-05-30 20:24:43 +0530

    error final

--- app/src/main/java/com/nithishkumar/seatplop/MainCourse/SeatStandActivity.java
+++ app/src/main/java/com/nithishkumar/seatplop/MainCourse/SeatStandActivity.java
@@ -1,15 +1,11 @@
 package com.nithishkumar.seatplop.MainCourse;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
-import android.view.View;
-import android.view.ViewGroup;
+import android.util.Log;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.nithishkumar.seatplop.R;
 
@@ -17,37 +13,41 @@ import java.util.ArrayList;
 
 public class SeatStandActivity extends AppCompatActivity {
 
-    ListView standListView;
-    ArrayList<String> stands;
-    ArrayAdapter arrayAdapter;
+    ListView seatStandListView;
+    //ArrayList<String> stands;
+    String[] stands = {"fdgdffg","sgg","faf"};
+    ArrayAdapter<String> arrayAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_seat_stand);
 
-        standListView = findViewById(R.id.stand_list_view);
-        stands = new ArrayList<>();
-        arrayAdapter = new ArrayAdapter(this,android.R.layout.simple_list_item_2,android.R.id.text1,stands) {
-            @NonNull
-            @Override
-            public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
-                View view = super.getView(position, convertView, parent);
-                TextView text1 = (TextView) view.findViewById(android.R.id.text1);
-                TextView text2 = (TextView) view.findViewById(android.R.id.text2);
+        seatStandListView = findViewById(R.id.stand_list_view_seatstandactivity);
+        //stands = new ArrayList<>();
+        arrayAdapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,stands);
 
-                text1.setText(stands.get(position));
-                text2.setText("40");
-                return view;
-            }
-        };
-        standListView.setAdapter(arrayAdapter);
+        /**stands.add("content");
+        stands.add("C Upper");
+        stands.add("C Lower");
+        stands.add("D Upper");
+        stands.add("D Lower");
+        stands.add("E Upper");
+        stands.add("E Lower");
+        stands.add("F Upper");
+        stands.add("F Lower");
+        stands.add("G Upper");
+        stands.add("G Lower");
+        stands.add("H Upper");
+        stands.add("H Lower");
 
-        addElementsToList();
+        stands.add("P.T Special Box");
+        stands.add("Terrace Bay");
+        stands.add("MCC Terrace");
+        stands.add("MCC Pavilion");
+        stands.add("Anna Pavilion");**/
 
-    }
-
-    private void addElementsToList() {
+        seatStandListView.setAdapter(arrayAdapter);
 
     }
 }
